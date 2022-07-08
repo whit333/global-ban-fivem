b362ed2fec05adf9042068bb86134786e562e1a4
whit333 <106171903+whit333@users.noreply.github.com>
2022-07-09 01:07:44 +0200

    Update server.lua
    ronin boşluklu kod mu yazılır %^%'!^!&!'^'!

--- fivem/server/server.lua
+++ fivem/server/server.lua
@@ -7,9 +7,7 @@ local function OnPlayerConnecting(name, setKickReason, deferrals)
 
   -- mandatory wait!
   Wait(0)
-
   deferrals.update(string.format("Hello %s. Your Steam ID is being checked.", name))
-
   for _, v in pairs(identifiers) do
       if string.find(v, "steam") then
           steamIdentifier = v
@@ -18,13 +16,8 @@ local function OnPlayerConnecting(name, setKickReason, deferrals)
         ip = v
       end
   end
-
   ip = ip:gsub("ip:", "")
-
-  print("ip: " .. ip, "steam: " .. steamIdentifier)
-
   Wait(0)
-
   PerformHttpRequest("http://localhost:3000/check/" .. ip .. "/" .. steamIdentifier, function(err, data, headers)
     if data == "true" then
       deferrals.done("girdi")
@@ -32,7 +25,6 @@ local function OnPlayerConnecting(name, setKickReason, deferrals)
       deferrals.done("Blacklistesin askim <3.")
     end
   end)
-
 end
 
 AddEventHandler("playerConnecting", OnPlayerConnecting)
@@ -40,7 +32,6 @@ AddEventHandler("playerConnecting", OnPlayerConnecting)
 RegisterCommand("blacklist", function(source,args)
   local ip = args[1]
   local steam = args[2]
-
   PerformHttpRequest("http://localhost:3000/addbanedplayer/" .. ip .. "/" .. steam, function(err, data, headers) end)
   print("added ip: " .. ip, "steam: " .. steam)
 end)
